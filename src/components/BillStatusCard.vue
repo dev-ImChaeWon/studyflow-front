0d742c192f62dc121d63ba4928de48f72d2f2493
임채원 <crown980129@gmail.com>
2024-11-02 00:04:28 +0900

    수납관리 수정

---
+++ src/components/BillStatusCard.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="card">
+    <div class="card-header">
+      <h2 class="student-name">김철수</h2>
+      <h3>납부일: 매달 30일</h3> <!-- 납부일: {{ ref }} -->
+      <h3>납부여부: O</h3> <!-- 납부여부: {{ ref }} -->
+    </div>
+    <div class="card-body">
+      <div class="bill-container">
+        <div class="subject-container">
+        <h4>초6수학</h4>
+          <span class="status-icon">
+          <svg width="20px" height="20px" viewBox="0 0 1024 1024" class="icon" version="1.1"
+            xmlns="http://www.w3.org/2000/svg">
+            <path
+              d="M511.9 183.1c-181.8 0-329.1 147.4-329.1 329.1 0 181.8 147.4 329.1 329.1 329.1C693.6 841.3 841 694 841 512.2S693.6 183.1 511.9 183.1z m0 585.1c-141.2 0-256-114.8-256-256s114.8-256 256-256 256 114.8 256 256-114.9 256-256 256z"
+              fill="green" />
+            <path d="M487.4 556.8l-97.8-87.6-48.8 54.4 153.1 137.2 192.2-221.2-55.2-48z" fill="green" /></svg>
+          </span>
+        </div>
+        <div class="subject-container">
+        <h4>영어</h4>
+          <span class="status-icon">
+          <svg width="20px" height="20px" viewBox="0 0 1024 1024" class="icon" version="1.1"
+            xmlns="http://www.w3.org/2000/svg">
+            <path
+              d="M511.9 183.1c-181.8 0-329.1 147.4-329.1 329.1 0 181.8 147.4 329.1 329.1 329.1C693.6 841.3 841 694 841 512.2S693.6 183.1 511.9 183.1z m0 585.1c-141.2 0-256-114.8-256-256s114.8-256 256-256 256 114.8 256 256-114.9 256-256 256z"
+              fill="green" />
+            <path d="M487.4 556.8l-97.8-87.6-48.8 54.4 153.1 137.2 192.2-221.2-55.2-48z" fill="green" /></svg>
+          </span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.card {
+  background-color: white;
+  border-radius: 8px;
+  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+  padding: 20px;
+
+}
+
+.card-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: baseline;
+}
+
+.card-body{
+  padding: 10px 0;
+  display: flex;
+  flex-direction: column;
+  row-gap: 15px;
+  font-size: 14px;
+}
+
+.subject-container {
+  margin-left: 20px;
+  display: grid;
+  grid-template-columns: auto auto 1fr;
+  column-gap: 10px;
+  justify-items: stretch;
+  row-gap: 5px;
+}
+
+</style>
--- src/components/tabs/BillManagement.vue
+++ src/components/tabs/BillManagement.vue
@@ -1,5 +1,116 @@
 <template>
     <h1>수납관리</h1>
+    <div class="filter-header">
+        
+    </div>
+    <div class="filter">
+        <div class="input-box">
+            <label for="teacher">과목</label>
+            <select class="filter-input" name="subject" id="subject">
+                <option value="all">전체</option>
+                <option v-for="s in subjects" :key="s.subjectId" :value="s.subjectId">{{s.subjectName}}</option>
+            </select>
+        </div>
+        <div class="input-box">
+            <label for="student-name">학생 : </label>
+            <input placeholder="Enter를 눌러 검색하세요" class="filter-input" id="student-name"/>
+        </div>
+    </div>
+
+    <CalendarTab/>
+    <BillStatusCard/>
 </template>
-<script setup></script>
-<style scoped></style>
+<script setup>
+import { onMounted, ref } from 'vue';
+import axios from 'axios';
+import BillStatusCard from '../BillStatusCard.vue';
+import CalendarTab from '../CalendarTab.vue';
+
+const subjects = ref([]);
+
+async function fetchSubjectList(){
+    try{
+        let res = await axios.get('http://localhost:8000/api/subject');
+        subjects.value = res.data;
+    }catch(e){
+        alert('서버에서 알 수 없는 오류가 발생했습니다. 잠시후 다시 시도해주세요');
+    }
+}
+
+onMounted(()=>{
+    fetchSubjectList();
+})
+
+</script>
+<style scoped>
+.filter-header{
+    padding: 25px 0 15px;
+}
+
+.tag-container{
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+}
+.filter{
+    display: flex;
+    align-items: stretch;
+    column-gap: 20px;
+    padding: 0 0 25px 0;
+}
+.input-box{
+    display: flex;
+    align-items: baseline;
+    column-gap: 5px;
+}
+
+
+label{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-shrink: 0;
+}
+.tags{
+    display: flex;
+    column-gap: 3px;
+    row-gap: 3px;
+    flex-wrap: wrap;
+}
+.icon-btn{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+    background-color: aliceblue;
+    border: none;
+    padding: 5px;
+    border-radius: 8px;
+    border: 0.5px solid rgb(135, 135, 247);
+    align-self: flex-end;
+}
+
+.filter-input{
+    padding: 5px;
+    border: none;
+    border-bottom:1px solid rgb(206, 206, 255);
+    outline: none;
+    
+}
+
+.input-box:last-child{
+    flex-grow: 1;
+}
+
+#student-name{
+    flex-grow: 1;
+    width: 100px;
+}
+
+.list-box{
+    padding: 20px 0;
+    display: flex;
+    flex-direction: column;
+    row-gap: 30px;
+}
+</style>
