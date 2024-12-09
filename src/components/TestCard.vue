07e73a4fdf8e185e12c8c7beaa1ebf42f630206b
임채원 <crown980129@gmail.com>
2024-12-09 14:23:54 +0900

    주간평가페이지

--- src/components/TestCard.vue
+++ src/components/TestCard.vue
@@ -1,16 +1,49 @@
 <template>
-  <div class="card">
+  <div v-if="studentSubject" class="card">
     <div class="card-header">
-      <h2 class="subject-name">중1수학</h2>
+      <h2 class="student-name">{{ studentSubject.student.studentName }}</h2>
+      <h2 class="subject-name">{{ studentSubject.subject.subjectName }}</h2>
     </div>
-    <div class="card-body">
-      
+    <div v-for="ts in testScores" :key="ts.id" class="card-body">
+      <div class="test-date">
+        <p class="info-text">응시일자</p>
+        <p>{{ ts.weeklyTestDate }}</p>
+      </div>
+      <div class="test-score">
+        <p class="info-text">점수</p>
+        <p>{{ ts.score }}</p>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup>
+import { ref, defineProps, onMounted } from 'vue';
+import axios from 'axios';
 
+const props = defineProps({
+  studentSubject: {
+    type: Object,
+    required: true, // 반드시 전달되는 값이어야 함
+  },
+});
+
+const testScores = ref([]);
+
+async function fetchTestScores(studentId, subjectId) {
+  try {
+    const res = await axios.get(`http://localhost:8000/api/all-test-score/${studentId}/${subjectId}`);
+    testScores.value = res.data;
+  } catch (e) {
+    console.error('시험점수정보를 가져오는데 실패했습니다: ', e);
+  }
+}
+
+onMounted(() => {
+  if (props.studentSubject) {
+    fetchTestScores(props.studentSubject.student.studentId, props.studentSubject.subject.subjectId);
+  }
+})
 </script>
 
 <style scoped>
@@ -20,19 +53,47 @@
   box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
   padding: 20px;
   margin: 20px;
+  font-family: Arial, sans-serif;
 }
 
 .card-header {
   display: flex;
   justify-content: space-between;
   align-items: baseline;
+  margin-bottom: 20px;
 }
 
-.card-body{
-  padding: 10px 0;
+.card-body {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 15px 0;
+  border-top: 1px solid #000;
+}
+
+.card-body .test-date,
+.card-body .test-score {
   display: flex;
   flex-direction: column;
-  row-gap: 15px;
+  text-align: center;
+  margin: 0;
+}
+
+.info-text {
+  font-weight: bold;
   font-size: 14px;
+  text-transform: uppercase;
+  color: #000;
+}
+
+.test-date p,
+.test-score p {
+  margin: 0;
+  font-size: 14px;
+  color: #000;
+}
+
+.test-score p {
+  font-weight: bold;
 }
 </style>
--- src/components/tabs/ParentTest.vue
+++ src/components/tabs/ParentTest.vue
@@ -1,18 +1,41 @@
 <template>
-  <h1>주간평가</h1>
-  <TestCard/>
+  <div class="test-score-header">
+    <h1>주간평가</h1>
+  </div>
+  <p v-if="!studentSubject">데이터를 불러오는 중...</p>
+  <TestCard v-if="studentSubject" :studentSubject="studentSubject" />
 </template>
 
 <script setup>
-// import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
 import TestCard from '../TestCard.vue';
 import { useRoute } from 'vue-router';
 
 let route = useRoute();
 console.log(route.params.id);
+const studentSubjectId = route.params.id;
+const studentSubject = ref(null);
+
+const fetchStudentByParent = async () => {
+  try {
+    const res = await axios.get(`http://localhost:8000/api/student-subject/${studentSubjectId}`)
+    studentSubject.value = res.data;
+  } catch (e) {
+    console.error('주간평가를 가져오는데 실패했습니다: ', e);
+  }
+}
+
+onMounted(()=>{
+  fetchStudentByParent();
+}
+
+)
 
 </script>
 
 <style scoped>
-
+.test-score-header {
+  padding: 0 20px;
+}
 </style>
